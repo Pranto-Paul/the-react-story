38d69864f4dc152d05703d873460d54d9685140e
pranto paul <prantopaul55555@gmail.com>
2026-01-02 20:27:59 +0600

    Add input box for setting counter value and update layout

--- 01_counter_project/src/App.jsx
+++ 01_counter_project/src/App.jsx
@@ -3,12 +3,16 @@ const App = () => {
   return (
     <div>
       <h2>Counter</h2>
-      <h5></h5>
+      <h4>56</h4>
 
       <div>
         <button>Incrase</button>
-        <button>Decrase</button>
+        <button style={{ margin: '10px' }}>Decrase</button>
         <button>Reset</button>
+      </div>
+      <div>
+        <input className="inputBox" type="text" name="" value={8} id="" />
+        <button>Set to 8</button>
       </div>
     </div>
   );

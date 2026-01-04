ba691b33d0b79741fece49b398d504442fdfea3f
pranto paul <prantopaul55555@gmail.com>
2026-01-04 17:00:00 +0600

    feat: add background color changer component with dynamic color buttons

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,11 +1,12 @@
+import { useState } from "react"
 import ColorName from "./constants"
 function App() {
-  
+  const [bgColor,setbgColor] = useState('red')
 
   return (
-    <div>
+    <div className="h-screen" style={{backgroundColor: bgColor}}>
     {ColorName.map((color,index) => (
-      <button key={index} style={{backgroundColor: color,margin: "5px",padding: "5px",borderRadius: "5px"}}>{color}</button>
+      <button key={index} onClick={() => setbgColor(color)} style={{backgroundColor: color,margin: "5px",padding: "5px",borderRadius: "5px"}}>{color}</button>
     ))}
     </div>
   )

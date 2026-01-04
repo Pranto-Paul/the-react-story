c6d70134108474b5a06bf67e4eeb81c9265dae8c
pranto paul <prantopaul55555@gmail.com>
2026-01-04 17:15:00 +0600

    feat: implement dynamic background color switcher using state and color constants

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react"
 import ColorName from "./constants"
 function App() {
-  const [bgColor,setbgColor] = useState('red')
+  const [bgColor,setbgColor] = useState('black')
 
   return (
     <div className="h-screen" style={{backgroundColor: bgColor}}>
+      <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-900/80 flex flex-wrap justify-center gap-3 p-4 rounded-2xl">
+
     {ColorName.map((color,index) => (
-      <button key={index} onClick={() => setbgColor(color)} style={{backgroundColor: color,margin: "5px",padding: "5px",borderRadius: "5px"}}>{color}</button>
+      <button key={index} onClick={() => setbgColor(color)} className="outline-none cursor-pointer capitalize mx-2 px-4 py-2 rounded-xl hover:scale-110 transition duration-300 ease-in-out" style={{backgroundColor: color, color: color == "black"? "white" : "black"}}>{color}</button>
     ))}
+      </div>
     </div>
   )
 }

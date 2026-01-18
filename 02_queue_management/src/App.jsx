945909c3e114f4c84de45c0ab292949d1873c12a
pranto paul <prantopaul55555@gmail.com>
2026-01-18 21:42:29 +0600

    Add QueueForm component and implement customer addition functionality

--- 02_queue_management/src/App.jsx
+++ 02_queue_management/src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+import QueueForm from './components/QueueForm';
+
 const App = () => {
+  const [queue, setQueue] = useState([]);
+  const addToQueue = (customer) => {
+    setQueue([...queue, { ...customer, id: Date.now(), status: 'waiting' }]);
+  };
+  const updateQueue = (id, customer) => {};
+  const removeFromQueue = (id) => {};
+
   return (
-    <div className="h-screen bg-slate-900 text-white">
-      <header className="text-center flex flex-col pt-3.5">
-        <h2 className="text-3xl font-bold leading-tight">
+    <div className="h-screen bg-slate-900 text-white flex flex-col">
+      <header className="text-center flex flex-col pt-6 pb-4 bg-slate-800 shadow-md">
+        <h2 className="text-3xl font-extrabold tracking-wide text-indigo-400">
           Queue Management Application
         </h2>
-        <p>Manage your customers efficiently</p>
+        <p className="text-slate-300 mt-1">Manage your customers efficiently</p>
       </header>
+      <main className="flex flex-1 p-6 gap-6">
+        <div className="flex-1 bg-slate-800 rounded-lg shadow-lg p-4">
+          <QueueForm onAdd={addToQueue} />
+        </div>
+        <div className="flex-3 bg-slate-800 rounded-lg shadow-lg p-4"></div>
+      </main>
     </div>
   );
 };
+
 export default App;

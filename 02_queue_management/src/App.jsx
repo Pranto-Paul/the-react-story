082daed9ef1841ee9cc8f2c445aa11115abeb43f
pranto paul <prantopaul55555@gmail.com>
2026-01-18 21:13:39 +0600

    initial commit from 02_queue_management_project

---
+++ 02_queue_management/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="h-screen bg-slate-900 text-white">
+      <header className="text-center flex flex-col pt-3.5">
+        <h2 className="text-3xl font-bold leading-tight">
+          Queue Management Application
+        </h2>
+        <p>Manage your customers efficiently</p>
+      </header>
+    </div>
+  );
+};
+export default App;

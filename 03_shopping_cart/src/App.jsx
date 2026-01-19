002016d52c9aea64a6da6bfe4ffd2a77336bab45
pranto paul <prantopaul55555@gmail.com>
2026-01-19 10:37:52 +0600

    initial commit from 03_shopping_cart

---
+++ 03_shopping_cart/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="bg-slate-800 text-white w-full h-screen">
+      <header className="p-7 border-b-2 border-slate-500 shadow-md">
+        <h2 className="font-bold text-3xl">Shopping Cart</h2>
+      </header>
+    </div>
+  );
+};
+export default App;
